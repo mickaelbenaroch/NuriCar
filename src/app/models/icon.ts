02617f883919055b14b8445a13661ea4c16b9dd2
mbenaroc <mickael_benaroch@bmc.com>
2021-01-05 15:18:08 +0200

    workflow

--- src/app/models/icon.ts
+++ src/app/models/icon.ts
@@ -1,5 +1,14 @@
+import {IconLevel} from '../enums/icon-level'
+
 export class Icon {
+
     public Description: string;
 
     public path: string;
+
+    public severity: IconLevel;
+
+    public suggestion: string;
+
+    public more: any;
 }
